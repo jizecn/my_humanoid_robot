d45536d196b2367cd9b0fd859283e6da9e700772
jizecn <jizecn@gmail.com>
2012-10-22 10:57:17 +0100

    clean

--- src/actionDecision/src/ActionDecision.cpp
+++ src/actionDecision/src/ActionDecision.cpp
@@ -45,89 +45,80 @@ bool ActionDecision::updateTouchEvent(ClassificationEvent &ce)
   //}
   //    eventActMap;
   
-  if(events.size() > 0)
-    {
-      //  map<SinglePartEvent, ActionCommand, compareEvent>::iterator it;
+  if(events.size() > 0) {
+    //  map<SinglePartEvent, ActionCommand, compareEvent>::iterator it;
+    
+    for(int k = 0; k < events.size(); k++) {
+      SinglePartEvent spe = *events[k];
       
-      for(int k = 0; k < events.size(); k++)
-	{
-	  SinglePartEvent spe = *events[k];
+      cout << "Event Received: " << spe.getPartName() << "  " << spe.getTouchType() << endl;
+      
+      for(int i = 0; i < eventActPairs.size(); i++) {
+	ActionCommand cmd;
+	std::string touchType = spe.getTouchType();
+	std::transform(touchType.begin(), touchType.end(),touchType.begin(), ::toupper);
+	if(touchType == "" || touchType == "NONE") {
+	  //if( eventActPairs[i].getEventKey().getPartName() == "lefthand" || eventActPairs[i].getEventKey().getPartName() == "righthand")
+	  if(spe.getPartName() == "lefthand" || spe.getPartName() == "righthand") {
+	    //cmd.setComplianceAction(ActionCommand::COMPLIANT, eventActPairs[i].getEventKey().getPartName());
+	    cmd.setComplianceAction(ActionCommand::RIGID, spe.getPartName());
+	  }
+	  yarp::os::Bottle &b = actionOutPort.prepare();
+	  std::string toSend = cmd.toString().c_str();
+	  cout << "Send ActionCommand --> " << toSend << endl;
+	  b.clear();
+	  b.add("(msg_type touch_action_command)");
+	  b.add(yarp::os::Value(toSend.c_str()));
+	  actionOutPort.write();
+	  return true;
+	}
+	
+	if(eventActPairs[i].ifSameKey(spe)) {
+	  cout << ">>>>> SinglePartEvent found in database" << endl;
 	  
-	  cout << "Event Received: " << spe.getPartName() << "  " << spe.getTouchType() << endl;
-	  
-	  for(int i = 0; i < eventActPairs.size(); i++)
-	    {
-	      ActionCommand cmd;
-	      std::string touchType = spe.getTouchType();
-	      std::transform(touchType.begin(), touchType.end(),touchType.begin(), ::toupper);
-	      if(touchType == "" || touchType == "NONE")
-		{
-		  //if( eventActPairs[i].getEventKey().getPartName() == "lefthand" || eventActPairs[i].getEventKey().getPartName() == "righthand")
-		  if(spe.getPartName() == "lefthand" || spe.getPartName() == "righthand")
-		    {
-		      //cmd.setComplianceAction(ActionCommand::COMPLIANT, eventActPairs[i].getEventKey().getPartName());
-		      cmd.setComplianceAction(ActionCommand::RIGID, spe.getPartName());
-		      //std::cout << "=====================" << spe.getPartName() << std::endl; 
-		    }
-		  yarp::os::Bottle &b = actionOutPort.prepare();
-		  std::string toSend = cmd.toString().c_str();
-		  cout << "Send ActionCommand --> " << toSend << endl;
-		  b.clear();
-		  b.add("(msg_type touch_action_command)");
-		  b.add(yarp::os::Value(toSend.c_str()));
-		  actionOutPort.write();
-		  return true;
-		}
-	      
-	      if(eventActPairs[i].ifSameKey(spe))
-		{
-		  cout << ">>>>> SinglePartEvent found in database" << endl;
-		  
-		  if(eventActPairs[i].decideActionCommand(cmd))
-		    {
-		      //    cout << "Action Command sent:  " << cmd.toString() << endl;
-		      
-		      cout << spe.getTouchType() << endl;
-		      
-		      if(spe.getTouchType().find("EXTENDED") != std::string::npos)
-			{
-			  if( eventActPairs[i].getEventKey().getPartName() == "lefthand" || eventActPairs[i].getEventKey().getPartName() == "righthand")
-			    cmd.setComplianceAction(ActionCommand::COMPLIANT, eventActPairs[i].getEventKey().getPartName());
-			}
-		      yarp::os::Bottle &b = actionOutPort.prepare();
-		      std::string toSend = cmd.toString().c_str();
-		      cout << "Send ActionCommand --> " << toSend << endl;
-		      b.clear();
-		      b.add("(msg_type touch_action_command)");
-		      b.add(yarp::os::Value(toSend.c_str()));
-		      actionOutPort.write();
-						return true;
-		    }
-		}
+	  if(eventActPairs[i].decideActionCommand(cmd)) {
+	    //    cout << "Action Command sent:  " << cmd.toString() << endl;
+	    
+	    cout << spe.getTouchType() << endl;
+	    
+	    if(spe.getTouchType().find("EXTENDED") != std::string::npos) {
+	      if( eventActPairs[i].getEventKey().getPartName() == "lefthand" || eventActPairs[i].getEventKey().getPartName() == "righthand")
+		cmd.setComplianceAction(ActionCommand::COMPLIANT, eventActPairs[i].getEventKey().getPartName());
 	    }
+	    yarp::os::Bottle &b = actionOutPort.prepare();
+	    std::string toSend = cmd.toString().c_str();
+	    cout << "Send ActionCommand --> " << toSend << endl;
+	    b.clear();
+	    b.add("(msg_type touch_action_command)");
+	    b.add(yarp::os::Value(toSend.c_str()));
+	    actionOutPort.write();
+	    return true;
+	  }
 	}
-      //     it = eventActMap.find(spe);
-      
-      /*
-	if(it != eventActMap.end())
-	{
-	ActionCommand cmd = (*it).second;
-	
-	yarp::os::Bottle &b = actionOutPort.prepare();
-	
-	string toSend = cmd.toString().c_str();
-	cout << "Send ActionCommand --> " << toSend << endl;
-	b.add(yarp::os::Value(toSend.c_str()));
-	actionOutPort.write();
-	}
-	else
-	{
-	cout << "No ActionCommand found" << endl;
-	
-	}
-      */
-      
+      }
     }
+    //     it = eventActMap.find(spe);
+    
+    /*
+      if(it != eventActMap.end())
+      {
+      ActionCommand cmd = (*it).second;
+      
+      yarp::os::Bottle &b = actionOutPort.prepare();
+      
+      string toSend = cmd.toString().c_str();
+      cout << "Send ActionCommand --> " << toSend << endl;
+      b.add(yarp::os::Value(toSend.c_str()));
+      actionOutPort.write();
+      }
+      else
+      {
+      cout << "No ActionCommand found" << endl;
+      
+      }
+    */
+    
+  }
   
   return true;
 }
--- src/actionDecision/src/KeyboardEvent.cpp
+++ src/actionDecision/src/KeyboardEvent.cpp
@@ -17,31 +17,30 @@ bool KeyboardEvent::init(Bottle &event)
 {
   this->event = event;
   int size = event.size();
-  if(size < 2)
-    {
-      cerr << "ERROR: received unrecognized command (keyboard command)" << endl;
-      return false;
-    }
+  if(size < 2) {
+    cerr << "ERROR: received unrecognized command (keyboard command)" << endl;
+    return false;
+  }
   //  yarp::os::Value prop = event.get(0).find("num_of_keys");
   //  if(prop.isNull())
   //{
-    //cerr << "WARNING: no number of keys information in the keyboard event" << endl;
-    //cerr << " force to use the content. no error checking here" << endl;
-      // this->numOfKeys = size - 1;
+  //cerr << "WARNING: no number of keys information in the keyboard event" << endl;
+  //cerr << " force to use the content. no error checking here" << endl;
+  // this->numOfKeys = size - 1;
   //}
   //else
   //{
-    //this->numOfKeys = prop.asInt();
-    // if(numOfKeys != size - 1)
-    // {
-    //      cerr << "ERROR: received unrecognized command (keyboard command)" << endl;
-    //      return false;
-    //  }
+  //this->numOfKeys = prop.asInt();
+  // if(numOfKeys != size - 1)
+  // {
+  //      cerr << "ERROR: received unrecognized command (keyboard command)" << endl;
+  //      return false;
+  //  }
   //}
   /*
-  for (int i = 0; i < this->numOfKeys; i++)
+    for (int i = 0; i < this->numOfKeys; i++)
     {
-      this->cmdList.push_back(event.get(i+1).asString().c_str());
+    this->cmdList.push_back(event.get(i+1).asString().c_str());
     }
   */
   this->setEvent(event.get(1).asString().c_str());
